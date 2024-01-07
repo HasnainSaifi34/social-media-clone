296aeb32fff5ecfbd416c3ca823f2bd644eb0f50
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-01-07 20:18:58 +0530

    added a seperate file for user realted resolvers

--- BackEnd/model/resolvers.js
+++ BackEnd/model/resolvers.js
@@ -1,41 +1,6 @@
-const {pool} = require('./pool')
-
-// Function to get a user by ID
-const getUserById = async (UserId) => {
-    const query = 'SELECT * FROM userAuth WHERE UserId = $1';
-    const result = await pool.query(query, [UserId]);
-    return result.rows[0];
-  };
-  
-  // Function to get all users
-  const getAllUsers = async () => {
-    const query = 'SELECT * FROM userAuth';
-    const result = await pool.query(query);
-    console.log('Result:', result.rows);
-    return result.rows;
-  };
-  
-  // Function to create a new user
-  const createUser = async (args) => {
-    const {
-      UserName,
-      FirstName,
-      LastName,
-      PhoneNo,
-      jwtToken,
-      PassWord,
-      ProfilePicture,
-      Age,
-    } = args;
-  
-    const query =
-      'INSERT INTO userAuth (UserName, FirstName, LastName, PhoneNo, jwtToken, PassWord, ProfilePicture, Age) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
-  
-    const values = [UserName, FirstName, LastName, PhoneNo, jwtToken, PassWord, ProfilePicture, Age];
-  
-    const result = await pool.query(query, values);
-    return result.rows[0];
-  };
+const { getUserById,
+  getAllUsers,
+  createUser,} =require('../controller/userResolver');
   const resolvers = {
     Query: {
       getUserById:(_, { UserId }) => getUserById(UserId),
@@ -55,8 +20,6 @@ const getUserById = async (UserId) => {
   };
   
   module.exports = {
-    getUserById,
-    getAllUsers,
-    createUser,
+    
     resolvers
   };
