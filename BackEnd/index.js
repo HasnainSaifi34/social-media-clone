ab765b942e372d2708bcf8f2341dfda1f1ef8756
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-01-07 18:03:07 +0530

    basic setup of apollo server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const server = express();
+
+const {ApolloServer}=require('@apollo/server');
+const {expressMiddleware} = require('@apollo/server/express4')
 const port = 3050;
+const cors = require('cors')
 require('dotenv').config();
-const pool = require('./model/pool.js')
-server.listen(port,()=>{console.log("server has started ")});
-server.get('/', async (req, res) => {
-    console.log(`server is running on port ${port}`);
-    res.send(`<h1>server is running<h1/>`);
-});
+const pool = require('./model/pool.js');
 
-server.get('/user', async(req,res)=>{res.send("you are a user")})
+const StartServer = async ()=>{
+    const express = require('express');
+    const server = express();
+    const apolloServer = new ApolloServer({
+        typeDefs:{},
+        resolvers:{}
+    
+    })
+    await apolloServer.start();
+    server.use(express.json());
+    server/use(cors());
+    server.use('/grahql',expressMiddleware(apolloServer));
+    server.listen(port,()=>{console.log("server has started ")});
+    server.get('/', async (req, res) => {
+        console.log(`server is running on port ${port}`);
+        res.send(`<h1>server is running<h1/>`);
+    });
+    
+    server.get('/user', async(req,res)=>{res.send("you are a user")})
+}
 
+StartServer();
 
 
