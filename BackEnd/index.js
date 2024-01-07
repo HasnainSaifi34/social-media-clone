5f554fc1ea2a1cd4a18ec00602e86fee877c0fc7
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-01-07 14:19:01 +0530

    added graph ql and apollo server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,6 +6,10 @@ const pool = require('./model/pool.js')
 server.listen(port,()=>{console.log("server has started ")});
 server.get('/', async (req, res) => {
     console.log(`server is running on port ${port}`);
-    res.send(`server is running`);
+    res.send(`<h1>server is running<h1/>`);
 });
+
+server.get('/user', async(req,res)=>{res.send("you are a user")})
+
+
 
