b933d23ece04037293cc29118ecb6339ffa56ece
HasnainSaifi34 <hasnainsaifi5670@gmail.com>
2024-01-09 00:26:08 +0530

    added a test query that is working using apolo client

--- FrontEnd/social-media-clone/app/login/page.tsx
+++ FrontEnd/social-media-clone/app/login/page.tsx
@@ -1,16 +1,45 @@
-import type { NextPage } from "next"
+"use client"
+import { NextPage } from "next";
+import { useQuery, gql } from '@apollo/client';
+import { useEffect, useState } from "react";
 
+const getUsers = gql`
+  query GetUsers {
+    getUsers {
+      firstname
+      lastname
+      age
+      password
+      jwttoken
+      
+    }
+  }
+`;
 
+const login: NextPage = () => {
+  const { loading, error, data } = useQuery(getUsers);
 
-const login:NextPage = ()=>{
-    return(
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    if (!loading && data) {
+      setUserData(data);
+      console.log(data);
+    }
+  }, [loading, data]);
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {userData && (
         <>
-        <h1>
-            this is a login page
-        </h1>
+          <h1>Data is loaded</h1>
+          {console.log(userData)}
         </>
-    )
-
-}
+      )}
+    </>
+  );
+};
 
 export default login;
